aa089de7b70b6287d9cd89e07f0425f25a130f82
Makaveli98 <renato.daman1998@gmail.com>
2020-01-20 11:11:04 +0100

    first commit

---
+++ JavaScript/Opdracht 1/Opdracht1,3.js
@@ -0,0 +1,7 @@
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    wielen: 4,
+    kleur: "blauw",
+}
+console.log(mijnauto.kleur);
---
+++ JavaScript/Opdracht 2/Opdracht 2.js
@@ -0,0 +1,12 @@
+document.getElementById("paginatitel").innerText = "Mijn Pagina";
+document.getElementById("subkop").innerText = "SubKop";
+document.getElementById("aap1"). src = "../../HTML/Img/SageMode.jpg";
+document.getElementById("aap1"). style.width = "100px";
+document.getElementById("aap2"). src = "../../HTML/Img/SasukeUchiha.jpg";
+document.getElementById("aap2"). style.width = "100px";
+
+document.getElementById("paginatitel").style.backgroundColor = "blue";
+document.getElementById("subkop").style.backgroundColor = "green";
+document.getElementById("text").style.backgroundColor = "yellow";
+document.getElementById("image").style.backgroundColor = "red";
+
